0824bbf6a683b665e41da8477f7cb8e329d407b2
Armando <jose.dlcruz.ayala@gmail.com>
2018-10-29 22:42:54 -0500

    howConnectPagesES6
    como exportar info de una pagina de codigo a otra

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,13 @@
+import str from './models/Search';
+// all usar el as, el nombre que le das es el que se usara en la pagina que estes
+// trabajando
+// import{add as a, multiply as m, ID} from './views/searchView';
 
-import x from './test';
-const y = 23;
-console.log(`i imported ${x} from anothr module variable y is ${y}`);
+// console.log(`Using imported functions! ${a(ID, 2)} and ${m(3,5)}. ${str}`);
+
+// otra forma de importar
+// llamas al archivo en si , pero a la hora de usar los elementos que este contiene
+// debes llamarlos, como si de un objeto se tratase
+import * as searchView from './views/searchView';
+
+console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}. ${str}`);
