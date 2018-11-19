ea6d5be352736773c46c068f2d20a0de5b557214
Armando <jose.dlcruz.ayala@gmail.com>
2018-11-19 18:30:09 -0500

    changes

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,4 @@
-import str from './models/Search';
+// import str from './models/Search';
 // all usar el as, el nombre que le das es el que se usara en la pagina que estes
 // trabajando
 // import{add as a, multiply as m, ID} from './views/searchView';
@@ -8,6 +8,35 @@ import str from './models/Search';
 // otra forma de importar
 // llamas al archivo en si , pero a la hora de usar los elementos que este contiene
 // debes llamarlos, como si de un objeto se tratase
-import * as searchView from './views/searchView';
+// import * as searchView from './views/searchView';
 
-console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}. ${str}`);
+// console.log(`Using imported functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3,5)}. ${str}`);
+
+// callFix http://cors-anywhere.herokuapp.com/
+// key a99a003d48f1747bda0c426443dc5a40 
+// url https://www.food2fork.com/api/search
+import Search from './models/Search';
+
+const state = {};
+
+const controlSearch =async () =>{
+    const query = 'pizza' //todo
+
+    if(query){
+        // 2)new search object and add to state
+        state.search = new Search(query);
+
+        // 3 prepare
+
+        // 4 search recipes
+        await state.search.getResults();
+
+        // 5 render results on ui
+        console.log(state.search.result)
+    }
+}
+
+document.querySelector('.search').addEventListener('submit',e=>{
+    e.preventDefault();
+    controlSearch();
+});
--- src/js/models/Search.js
+++ src/js/models/Search.js
@@ -1,1 +1,19 @@
-export default 'I am an exported string.';
+// export default 'I am an exported string.';
+import axios from 'axios';
+export default class Search{
+    constructor(query){
+        this.query = query;
+    }
+
+    async getResults(){
+        const proxy = 'http://cors-anywhere.herokuapp.com/';
+        const key = 'a99a003d48f1747bda0c426443dc5a40';
+        try{
+            const res = await axios(`${proxy}https://www.food2fork.com/api/search?key=${key}&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch(error){
+            alert(error)
+        }
+    }
+}
